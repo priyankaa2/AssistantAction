f4f81ab0b13222e5fbe6108c5f1fb8031107eb73
Priyanka Shah <PriyankaShah@priyankas-mbp.lan>
2019-04-22 11:03:54 -0700

    sending

--- SnipsClients/SnipsMPU.py
+++ SnipsClients/SnipsMPU.py
@@ -54,8 +54,8 @@ class SnipsMPU(object):
         print("Light Turned On")
         self.__relay.turn_on()
         hermes.publish_end_session(intent_message.session_id,self.__i18n.get('relayTurnOn'))
-        response_lightson = requests.get('http://192.168.87.24:8081/sunits/lights_on')
-        print(response_lightson)
+        #response_lightson = requests.get('http://192.168.87.24:8081/sunits/lights_on')
+        #print(response_lightson)
 
     @check_confidence_score
     @check_site_id
@@ -63,8 +63,8 @@ class SnipsMPU(object):
         print("Light Turned Off")
         self.__relay.turn_off()
         hermes.publish_end_session(intent_message.session_id,self.__i18n.get('relayTurnOff'))
-        response_lightsoff = requests.get('http://192.168.87.24:8081/sunits/lights_off')
-        print(response_lightsoff)
+        #response_lightsoff = requests.get('http://192.168.87.24:8081/sunits/lights_off')
+        #print(response_lightsoff)
 
     @check_confidence_score
     @check_site_id
@@ -72,9 +72,9 @@ class SnipsMPU(object):
         print("Get Unit")
         self.__relay.unit_get()
         hermes.publish_end_session(intent_message.session_id, "Getting the bed")
-        response_get_bed = requests.post('http://192.168.87.24:8081/sunits/switch_mode', json={'mode':'night'})
-        json_response_get_bed = response_get_bed.json()
-        print(json_response_get_bed)
+        #response_get_bed = requests.post('http://192.168.87.24:8081/sunits/switch_mode', json={'mode':'night'})
+        #json_response_get_bed = response_get_bed.json()
+        #print(json_response_get_bed)
         #house_room = intent_message.slots.house_room.first().value # We extract the value from the slot "house_room"
 
     @check_confidence_score
@@ -83,9 +83,9 @@ class SnipsMPU(object):
         print("Take Unit")
         self.__relay.unit_take()
         hermes.publish_end_session(intent_message.session_id,"Taking your unit.")
-        response_raise_all = requests.post('http://192.168.87.24:8081/sunits/raise_all', json={'speed_factor':'3000'})
-        json_response_raise_all = response_raise_all.json()
-        print(json_response_raise_all)
+        #response_raise_all = requests.post('http://192.168.87.24:8081/sunits/raise_all', json={'speed_factor':'3000'})
+        #json_response_raise_all = response_raise_all.json()
+        #print(json_response_raise_all)
 
     @check_confidence_score
     @check_site_id
